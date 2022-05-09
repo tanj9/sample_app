9979aaa85607f7bc22134d163c01693724139186
Jerome Tan <jerome.tan@tuta.io>
2022-05-09 16:10:55 +0200

    added password reset tests

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -16,7 +16,7 @@ class PasswordResetsController < ApplicationController
       redirect_to root_url
     else
       message = 'Email address not found.'
-      flash.now[:danger] = message
+      flash[:danger] = message
       render 'new'
     end
   end
@@ -32,6 +32,7 @@ class PasswordResetsController < ApplicationController
     elsif @user.update(user_params)
       # Case 4: successful update
       log_in @user
+      @user.update_attribute(:reset_digest, nil)
       flash[:success] = 'Password successfully reset.'
       redirect_to @user
     else
@@ -39,8 +40,6 @@ class PasswordResetsController < ApplicationController
       render 'edit'
     end
   end
-
-  # http://localhost:3000/password_resets/8VjJYPs09-LQzAsx5atG7Q/edit?email=natoaix%40gmail.com
 
   private
 
@@ -62,10 +61,10 @@ class PasswordResetsController < ApplicationController
 
   # Check if password reset token has expired
   def check_expiration
-    if @user.password_reset_expired?
-      message = 'Password reset has expired. Please renew your reset request.'
-      flash.now[:danger] = message
-      redirect_to new_password_reset_url
-    end
+    return unless @user.password_reset_expired?
+
+    redirect_to new_password_reset_url
+    message = 'Password reset has expired. Please renew your password reset request.'
+    flash[:danger] = message
   end
 end
