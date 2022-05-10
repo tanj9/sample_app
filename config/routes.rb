6bf354f5a47ddf9e412c94e9adab70810d6f9e71
Jerome Tan <jerome.tan@tuta.io>
2022-05-10 23:24:44 +0200

    added relationship stats on home page and routes to following and followers indexes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
