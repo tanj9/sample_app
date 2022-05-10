fe70eebb50b6d4633d34322aaa90b4aeaeed3e5e
Jerome Tan <jerome.tan@tuta.io>
2022-05-10 16:30:37 +0200

    switched back to development config for mailer (sendgrid topic)

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,40 +5,40 @@ class UserMailer < ApplicationMailer
   def account_activation(user)
     @user = user
     # version for test / development (using #mail method)
-    # mail to: user.email, subject: "Account activation (Sample App)"
+    mail to: user.email, subject: "Account activation (Sample App)"
 
     # version for production / Heroku involing SENDGRID
-    from = SendGrid::Email.new(email: 'jerome.tan@tuta.io')
-    to = SendGrid::Email.new(email: user.email)
-    subject = 'Account activation (Sample App)'
-    content = Content.new(type: 'text/html', value: render('user_mailer/account_activation.html.erb'))
-    mail = SendGrid::Mail.new(from, subject, to, content)
+    # from = SendGrid::Email.new(email: 'jerome.tan@tuta.io')
+    # to = SendGrid::Email.new(email: user.email)
+    # subject = 'Account activation (Sample App)'
+    # content = Content.new(type: 'text/html', value: render('user_mailer/account_activation.html.erb'))
+    # mail = SendGrid::Mail.new(from, subject, to, content)
 
-    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-    response = sg.client.mail._('send').post(request_body: mail.to_json)
-    puts response.status_code
-    puts response.body
-    # puts response.parsed_body
-    puts response.headers
+    # sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    # response = sg.client.mail._('send').post(request_body: mail.to_json)
+    # puts response.status_code
+    # puts response.body
+    # # puts response.parsed_body
+    # puts response.headers
   end
 
   def password_reset(user)
     @user = user
     # version for test / development (using #mail method)
-    # mail to: user.email, subject: "Password reset (Sample App)"
+    mail to: user.email, subject: "Password reset (Sample App)"
 
     # version for production / Heroku involing SENDGRID
-    from = SendGrid::Email.new(email: 'jerome.tan@tuta.io')
-    to = SendGrid::Email.new(email: user.email)
-    subject = 'Password reset (Sample App)'
-    content = Content.new(type: 'text/html', value: render('user_mailer/password_reset.html.erb'))
-    mail = SendGrid::Mail.new(from, subject, to, content)
+    # from = SendGrid::Email.new(email: 'jerome.tan@tuta.io')
+    # to = SendGrid::Email.new(email: user.email)
+    # subject = 'Password reset (Sample App)'
+    # content = Content.new(type: 'text/html', value: render('user_mailer/password_reset.html.erb'))
+    # mail = SendGrid::Mail.new(from, subject, to, content)
 
-    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-    response = sg.client.mail._('send').post(request_body: mail.to_json)
-    puts response.status_code
-    puts response.body
-    # puts response.parsed_body
-    puts response.headers
+    # sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    # response = sg.client.mail._('send').post(request_body: mail.to_json)
+    # puts response.status_code
+    # puts response.body
+    # # puts response.parsed_body
+    # puts response.headers
   end
 end
