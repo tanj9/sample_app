5127267efb308a4efb52af897d11b50bc4f68fdb
Jerome Tan <jerome.tan@tuta.io>
2022-05-10 12:39:31 +0200

    microposts controller destroy action with preliminary validation that current user owns the micropost to be destroyed

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: %w[create destroy]
+  before_action :correct_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -12,11 +13,21 @@ class MicropostsController < ApplicationController
     end
   end
 
-  def destroy; end
+  def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost deleted."
+    redirect_to request.referrer || root_url
+  end
 
   private
 
   def micropost_params
     params.require(:micropost).permit(:content)
+  end
+
+  # Redirect to Home if current user does not own the micropost to be destroyed
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
   end
 end
