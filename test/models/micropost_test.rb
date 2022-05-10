bc576657b2242fe75316322dd01091bf55bd7f25
Jerome Tan <jerome.tan@tuta.io>
2022-05-10 09:35:37 +0200

    micropost model tests

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:jerome)
+    @micropost = Micropost.new(content: 'Lorem Ipsum', user_id: @user.id)
+  end
+
+  test 'should be valid' do
+    assert @micropost.valid?
+  end
+
+  test 'user id should be present' do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
+  test 'content should be present' do
+    @micropost.content = "      "
+    assert_not @micropost.valid?
+  end
+
+  test 'content should not exceed 140 chars' do
+    @micropost.content = 'a' * 141
+    assert_not @micropost.valid?
+  end
 end
