7ad82660e9103baab0dff097d6dc1e5c22904083
Jerome Tan <jerome.tan@tuta.io>
2022-05-11 16:22:58 +0200

    added ajax on follow and unfollow buttons (unsuccessfully, though)

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,14 +2,20 @@ class RelationshipsController < ApplicationController
   before_action :logged_in_user
 
   def create
-    user_to_follow = User.find(params[:followed_id])
-    current_user.follow(user_to_follow)
-    redirect_to user_to_follow
+    @user_to_follow = User.find(params[:followed_id])
+    current_user.follow(@user_to_follow)
+    respond_to do |format|
+      format.html { redirect_to @user_to_follow }
+      format.js
+    end
   end
 
   def destroy
-    user_to_unfollow = Relationship.find(params[:id]).followed
-    current_user.unfollow(user_to_unfollow)
-    redirect_to user_to_unfollow
+    @user_to_unfollow = Relationship.find(params[:id]).followed
+    current_user.unfollow(@user_to_unfollow)
+    respond_to do |format|
+      format.html { redirect_to @user_to_unfollow }
+      format.js
+    end
   end
 end
