fb052048cabb8c949019c1fa92a20c68f7ab746c
Jerome Tan <jerome.tan@tuta.io>
2022-05-11 14:49:54 +0200

    added show_follow view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: %w[index edit update destroy]
+  before_action :logged_in_user, only: %w[index edit update destroy
+                                          following followers]
   before_action :correct_user, only: %w[edit update]
   before_action :admin_user, only: :destroy
 
@@ -47,6 +48,20 @@ class UsersController < ApplicationController
     User.find(params[:id]).destroy
     flash[:success] = 'User deleted'
     redirect_to users_path
+  end
+
+  def following
+    @title = 'following'
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = 'followers'
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
 
   private
