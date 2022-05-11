47c1a73ddac7d768d100a0748dbaa850431ba773
Jerome Tan <jerome.tan@tuta.io>
2022-05-11 17:03:39 +0200

    updated feed method in user model to include followed users' posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -82,9 +82,9 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
-  # Defines a proto-feed.
+  # Returns a user's status feed (microposts by followed users and self)
   def feed
-    Micropost.where("user_id = ?", id)
+    Micropost.where('user_id IN (?) OR user_id = ?', following_ids, id)
   end
 
   # Follows a user.
