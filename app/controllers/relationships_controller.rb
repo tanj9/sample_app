7b455dd1b5cba103ea7b2b5f389333d63e17ece7
Jerome Tan <jerome.tan@tuta.io>
2022-05-17 16:18:28 +0200

    fixed ajax bug and got ajax tests to pass

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,19 +2,19 @@ class RelationshipsController < ApplicationController
   before_action :logged_in_user
 
   def create
-    @user_to_follow = User.find(params[:followed_id])
-    current_user.follow(@user_to_follow)
+    @user = User.find(params[:followed_id])
+    current_user.follow(@user)
     respond_to do |format|
-      format.html { redirect_to @user_to_follow }
+      format.html { redirect_to @user }
       format.js
     end
   end
 
   def destroy
-    @user_to_unfollow = Relationship.find(params[:id]).followed
-    current_user.unfollow(@user_to_unfollow)
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow(@user)
     respond_to do |format|
-      format.html { redirect_to @user_to_unfollow }
+      format.html { redirect_to @user }
       format.js
     end
   end
--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -32,11 +32,11 @@ class FollowingTest < ActionDispatch::IntegrationTest
     end
   end
 
-  # test 'should follow a user with Ajax' do
-  #   assert_difference '@user.following.count', 1 do
-  #     post relationships_path, xhr: true, params: { followed_id: @other_user.id }
-  #   end
-  # end
+  test 'should follow a user with Ajax' do
+    assert_difference '@user.following.count', 1 do
+      post relationships_path, xhr: true, params: { followed_id: @other_user.id }
+    end
+  end
 
   test 'should unfollow a user the standard way' do
     @user.follow(@other_user)
@@ -46,13 +46,13 @@ class FollowingTest < ActionDispatch::IntegrationTest
     end
   end
 
-  # test 'should unfollow a user with Ajax' do
-  #   @user.follow(@other_user)
-  #   relationship = @user.active_relationships.find_by(followed_id: @other_user.id)
-  #   assert_difference '@user.following.count', -1 do
-  #     delete relationship_path(relationship), xhr: true
-  #   end
-  # end
+  test 'should unfollow a user with Ajax' do
+    @user.follow(@other_user)
+    relationship = @user.active_relationships.find_by(followed_id: @other_user.id)
+    assert_difference '@user.following.count', -1 do
+      delete relationship_path(relationship), xhr: true
+    end
+  end
 
   test 'first feed page should appear on home page' do
     get root_path
